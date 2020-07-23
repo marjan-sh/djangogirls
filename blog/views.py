21c79680a2cd3d802863cad220a747efe4d7563c
marjan <marjansh09@gmail.com>
2020-07-23 12:30:57 +0200

    Update

--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,6 @@ from django.shortcuts import render, get_object_or_404
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
-    Post.objects.get(pk=pk)
-
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -18,4 +15,4 @@ def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})    
 
-       
+    Post.objects.get(pk=pk)   
